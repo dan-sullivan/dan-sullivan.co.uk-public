cb572dd963966a72f166c0da498974d4cab19ab4
Dan Sullivan <dan.sullivan.dev@gmail.com>
2018-01-25 09:58:11 +0000

    added s3 backend for terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,10 @@
 terraform {
   required_version = ">= 0.10.1"
+  backend "s3" {
+    bucket = "dscouk-state"
+    key    = "prod/terraform.state"
+    region = "eu-west-2"
+  }
 }
 
 provider "aws" {
@@ -9,3 +14,4 @@ provider "aws" {
 # Use aws_caller_identity to get my AWS account ID for reference 
 # Used here instead of hardcoding or setting a var for the account ID in ARNs
 data "aws_caller_identity" "current" {}
+
